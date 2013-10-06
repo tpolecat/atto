09d488a7a7764b68d967e6a9343a0864138f0862
Rob Norris <rob_norris@mac.com>
2013-10-06 11:15:15 -0700

    signed numerics

--- src/main/scala/atto/parser/Numeric.scala
+++ src/main/scala/atto/parser/Numeric.scala
@@ -10,51 +10,55 @@ trait Numeric {
   import combinator._
   import text._
   import character._
-  
+
   ////// Integral
 
   /** Parser for an arbitrary-precision integer. */
   val bigInt: Parser[BigInt] =
-    stringOf(digit).map(BigInt.apply) as "bigInt"
+    (signum |@| stringOf(digit))(_ * BigInt.apply(_)) asOpaque "bigInt"
 
-  /** Parser for a Long. */
+  /** Parser for a Long (range-checked). */
   val long: Parser[Long] =
-    narrow(bigInt)(_.isValidLong, _.toLong) as "long"
+    narrow(bigInt)(_.isValidLong, _.toLong, "long")
 
-  /** Parser for an Int. */
+  /** Parser for an Int (range-checked). */
   val int: Parser[Int] =
-    narrow(bigInt)(_.isValidInt, _.toInt) as "int"
+    narrow(bigInt)(_.isValidInt, _.toInt, "int")
   
-  /** Parser for a Short. */
+  /** Parser for a Short (range-checked). */
   val short: Parser[Short] =
-    narrow(bigInt)(_.isValidShort, _.toShort) as "short"
+    narrow(bigInt)(_.isValidShort, _.toShort, "short")
 
-  /** Parser for a Byte. */
+  /** Parser for a Byte (range-checked). */
   val byte: Parser[Byte] =
-    narrow(bigInt)(_.isValidByte, _.toByte) as "byte"
+    narrow(bigInt)(_.isValidByte, _.toByte, "byte")
 
   ////// Real
 
   /** Parser for an arbitrary-precision decimal. */
   val bigDecimal: Parser[BigDecimal] = 
-    (stringOf(digit) |@| opt(char('.') ~> stringOf(digit))) {
-      case (a, Some(b)) => BigDecimal(s"$a.$b")
-      case (a, None) => BigDecimal(a)
+    (signum |@| stringOf(digit) |@| opt(char('.') ~> stringOf(digit))) {
+      case (s, a, Some(b)) => s * BigDecimal(s"$a.$b")
+      case (s, a, None)    => s * BigDecimal(a)
     } as "bigDecimal"
 
-  /** Parser for a Double. */
-  val double: Parser[Double] = // N.B. BigDecimal.isValidDouble is broken
-    narrow(bigDecimal)(bd => BigDecimal(bd.toDouble) == bd, _.toDouble) as "double"
+  /** Parser for a Double (unchecked narrowing). */
+  val double: Parser[Double] =
+    bigDecimal.map(_.toDouble) as "double"
 
-  /** Parser for a Float. */
-  val float: Parser[Float] = // N.B. BigDecimal.isValidFloat is broken
-    narrow(bigDecimal)(bd => BigDecimal(bd.toFloat.toDouble) == bd, _.toFloat) as "float"
+  /** Parser for a Float (unchecked narrowing). */
+  val float: Parser[Float] = 
+    bigDecimal.map(_.toFloat) as "float"
 
   ////// Helpers
 
-  private def narrow[A,B](p: Parser[A])(f: A => Boolean, g: A => B): Parser[B] =
+  /** Parser for a leading `+` or `-` as a signum, defaulting to `1` */
+  def signum: Parser[Int] =
+    char('+').map(_ => 1) | char('-').map(_ => -1) | ok(1)
+
+  private def narrow[A,B](p: Parser[A])(f: A => Boolean, g: A => B, as: String): Parser[B] =
     p flatMap { a => 
       if (f(a)) ok(g(a)) else err("too large, too small, or too precise: " + a)
-    }
+    } as as
 
 }

04e5cccc64165dfe5720e5a59bbe030cd01b375a
Rob Norris <rob_norris@mac.com>
2013-10-12 12:44:23 -0700

    added ushort and uint

--- src/main/scala/atto/parser/Spire.scala
+++ src/main/scala/atto/parser/Spire.scala
@@ -3,22 +3,38 @@ package parser
 
 import _root_.spire.math._
 import atto.syntax.parser._
+import scalaz.Order
+import scalaz.syntax.order._
 
 trait Spire {
   import combinator._
   import numeric._
 
+  /** Parser for an unsigned byte (range-checked). */
   val ubyte: Parser[UByte] = 
-    int.flatMap { n =>
-      if (n >= UByte.MinValue.toInt && n <= UByte.MaxValue.toInt) ok(UByte(n)) 
-      else err(s"out of range: $n)")
-    } as "ubyte"
+    narrow[Int, UByte](int, UByte.MinValue.toInt, UByte.MaxValue.toInt, UByte(_))
+
+  /** Parser for an unsigned short (range-checked). */
+  val ushort: Parser[UShort] = 
+    narrow[Int, UShort](int, UShort.MinValue.toInt, UShort.MaxValue.toInt, UShort(_))
+
+  /** Parser for an unsigned int (range-checked). */
+  val uint: Parser[UInt] = 
+    narrow[Long, UInt](long, UInt.MinValue.toLong, UInt.MaxValue.toLong, UInt(_))
+
+  // TODO: how do I construct a ULong larger than Long.MaxValue?
+  // /** Parser for an unsigned long (range-checked). */
+  // val ulong: Parser[ULong] = 
+  //   narrow[BigInt, ULong](bigInt, ULong.MinValue.toBigInt, ULong.MaxValue.toBigInt, ULong(_))
+
 
   ////// Helpers
 
-  // private def narrow[A,B](p: Parser[A])(f: A => Boolean, g: A => B, as: String): Parser[B] =
-  //   p flatMap { a => 
-  //     if (f(a)) ok(g(a)) else err("too large, too small, or too precise: " + a)
-  //   } as as
+  private def narrow[A : Order, B](pa: Parser[A], min: A, max: A, f: A => B): Parser[B] = 
+    pa.flatMap { n =>
+      if (n >= min && n <= max) ok(f(n)) 
+      else err(s"out of range: $n)")
+    } as "ubyte"
 
 }
+

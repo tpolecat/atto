243d69d26cf8e560cac414a8ad686e9f55131b64
Rob Norris <rob_norris@mac.com>
2014-04-11 22:39:27 -0700

    scaladoc, cleanup

--- core/src/main/scala/atto/parser/Text.scala
+++ core/src/main/scala/atto/parser/Text.scala
@@ -18,9 +18,11 @@ trait Text {
   import combinator._
   import character._
 
+  /** Parser that returns a string of characters matched by `p`. */
   def stringOf(p: Parser[Char]): Parser[String] =
     many(p).map(_.mkString) as "stringOf(" + p + ")"
 
+  /** Parser that returns a non-empty string of characters matched by `p`. */
   def stringOf1(p: Parser[Char]): Parser[String] =
     many1(p).map(_.mkString) as "stringOf1(" + p + ")"
 
@@ -38,9 +40,11 @@ trait Text {
   /** Like `string` but case-insensitive `s`. */
   def stringCI(s: String): Parser[String] = 
     take(s.length).filter(_ equalsIgnoreCase s) asOpaque "stringCI(\"" + s + "\")"
-
-  ////// FROM RUNAR
  
+  /** 
+   * Parser that returns a string of characters passing the supplied predicate. Equivalent to but
+   * more efficient than `stringOf(elem(p))`.
+   */
   def takeWhile(p: Char => Boolean): Parser[String] = {
     def go(acc: List[String]): Parser[List[String]] = for {
       x <- get
@@ -55,6 +59,20 @@ trait Text {
     go(Nil).map(_.reverse.concatenate)
   }
 
+ /** 
+   * Parser that returns a non-empty string of characters passing the supplied predicate. Equivalent 
+   * to but more efficient than `stringOf1(elem(p))`.
+   */
+  def takeWhile1(p: Char => Boolean): Parser[String] = for {
+    _ <- get map (_.isEmpty) flatMap (_.whenM(demandInput))
+    s <- get
+    (h, t) = s span p
+    _ <- h.isEmpty.whenM(err("takeWhile1"):Parser[Unit])
+    _ <- put(t)
+    r <- if (t.isEmpty) takeWhile(p).map(h + _) else ok(h)
+  } yield r
+
+  /** Parser that consumes and returns all remaining chunks of input. */
   lazy val takeRest: Parser[List[String]] = {
     def go(acc: List[String]): Parser[List[String]] = for {
       input <- wantInput
@@ -67,52 +85,44 @@ trait Text {
     go(Nil)
   }
 
+  /** Parser that consumes and returns all remaining input. */
   lazy val takeText: Parser[String] =
     takeRest map (_.concatenate)
 
-  def takeWhile1(p: Char => Boolean): Parser[String] = for {
-    _ <- get map (_.isEmpty) flatMap (_.whenM(demandInput))
-    s <- get
-    (h, t) = s span p
-    _ <- h.isEmpty.whenM(err("takeWhile1"):Parser[Unit])
-    _ <- put(t)
-    r <- if (t.isEmpty) takeWhile(p).map(h + _) else ok(h)
-  } yield r
-
-
-  sealed trait Scan[+S]
-  case class Continue[S](s: S) extends Scan[S]
-  case class Finished(n: Int, s: String) extends Scan[Nothing]
-
+  /** 
+   * Stateful scanning parser returning a string of all visited chars. Many combinators can be 
+   * written in terms of `scan`; for instance, `take(n)` could be written  as 
+   * `scan(n)((m, _) => m > 0 option m - 1)`.
+   */
   def scan[S](s: S)(p: (S, Char) => Option[S]): Parser[String] = {
+
+    sealed trait Scan[+S]
+    case class Continue[S](s: S) extends Scan[S]
+    case class Finished(n: Int, s: String) extends Scan[Nothing]
+
     def scanner(s: S, n: Int, t: String): Scan[S] = {
       if (t.isEmpty) Continue(s)
       else p(s, t.head) match {
         case Some(s) => scanner(s, n + 1, t.tail)
-        case None => Finished(n, t)
+        case None    => Finished(n, t)
       }
     }
+
     def go(acc: List[String], s: S): Parser[List[String]] = for {
       input <- get
       r <- scanner(s, 0, input) match {
         case Continue(sp) => for {
-          _ <- put("")
+          _    <- put("")
           more <- wantInput
-          r <- if (more) go(input :: acc, sp) else ok(input :: acc)
+          r    <- if (more) go(input :: acc, sp) else ok(input :: acc)
         } yield r
         case Finished(n, t) => put(t).flatMap(_ => ok(input.take(n) :: acc))
       }
     } yield r
-    for {
-      chunks <- go(Nil, s)
-      r <- chunks match {
-        case List(x) => ok(x)
-        case xs => ok(xs.reverse.concatenate)
-      }
-    } yield r
-  }
 
-  ////// MISC
+    go(Nil, s).map(_.reverse.concatenate)
+
+  }
 
   /** Quoted strings with control and unicode escapes, Java/JSON style. **/
   val stringLiteral: Parser[String] = {

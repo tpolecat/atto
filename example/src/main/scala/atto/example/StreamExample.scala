51118741e685848d49d88400978bc1d8e616ad6f
Rob Norris <rob_norris@mac.com>
2014-08-10 09:23:59 -0700

    disabled atto-stream for now

--- example/src/main/scala/atto/example/StreamExample.scala
+++ example/src/main/scala/atto/example/StreamExample.scala
@@ -1,22 +1,25 @@
-package atto.example
 
-import atto._
-import Atto._
-import atto.syntax.stream.all._
+// Excluded temporarily waiting on stream for 2.11+7.1
 
-import scala.App
-import scalaz.stream._
+// package atto.example
 
-object StreamExample extends App {
+// import atto._
+// import Atto._
+// import atto.syntax.stream.all._
 
-  // find and print the first 20 ints larger than 100 in /etc/services in constant space
-  io.linesR("/etc/services")
-    .parseLenient(int) // the new bit; the rest is vanilla streams
-    .filter(_ > 100)
-    .take(20)
-    .map(_.toString + " ")
-    .to(io.stdOut)
-    .run
-    .run
+// import scala.App
+// import scalaz.stream._
 
-}
+// object StreamExample extends App {
+
+//   // find and print the first 20 ints larger than 100 in /etc/services in constant space
+//   io.linesR("/etc/services")
+//     .parseLenient(int) // the new bit; the rest is vanilla streams
+//     .filter(_ > 100)
+//     .take(20)
+//     .map(_.toString + " ")
+//     .to(io.stdOut)
+//     .run
+//     .run
+
+// }
